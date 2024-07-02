2f0ab29d17764a6e8b0680dd4a66e5da8a22ad5e
RenatoPereirals <renatopreirals@gmail.com>
2024-07-01 23:33:58 -0300

    feat: add Event.cs to define the Event model

---
+++ Models/Event.cs
@@ -0,0 +1,29 @@
+﻿namespace EventMaster.Models;
+
+public class Event
+{
+    /// <summary>
+    /// The unique identifier for the event.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The name of the event. The event name must be unique and must not contain special characters 
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The date of the event.
+    /// </summary>
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// The location of the event.
+    /// </summary>
+    public string Location { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The list of speakers for the event.
+    /// </summary>
+    public List<string> Speakers { get; set; } = new();
+}
